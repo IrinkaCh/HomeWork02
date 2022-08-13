7e55674a7bf5da5d8ad1919235b81fbbc665b999
IrinkaCh <123irina00@mail.ru>
2022-08-13 23:06:17 +0500

    Task 00 Find Weekend

---
+++ Task03/FindWeekend/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели, от 1 до 7 ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void FindWeekend()
+{
+if((number == 6)^(number == 7)){Console.WriteLine("Да");}
+else{
+    if((number > 7)^(number < 1)){Console.WriteLine("Введите корректную цифру, от 1 до 7 ");}
+    else{ Console.WriteLine("Нет");}
+    }
+}
+FindWeekend();
