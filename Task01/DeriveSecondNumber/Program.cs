f9c5debb909f4d5595abad48d11dabfc8a5d0f0f
IrinkaCh <123irina00@mail.ru>
2022-08-13 22:49:37 +0500

    Task 01 Derive Second Number

---
+++ Task01/DeriveSecondNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void derivesecondnumber()
+{
+int result = number / 10 % 10;
+Console.WriteLine(result);
+}
+derivesecondnumber();
