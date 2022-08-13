1d3112d5627861791d8f02b5fdc6afbe63998fdd
IrinkaCh <123irina00@mail.ru>
2022-08-13 23:08:13 +0500

    Task 02 Find Third Number

--- Task02/FindThirdNumber/Program.cs
+++ Task02/FindThirdNumber/Program.cs
@@ -3,9 +3,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 string result = number.ToString();
 int i = 2;
 
-void three()
+void FindThirdNumber()
 {
 if(number / 100 > 0){Console.WriteLine(result[i]);}
 else{Console.WriteLine("Третьей цифры нет");}
 }
-three();
+FindThirdNumber();
